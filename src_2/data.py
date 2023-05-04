716dacb787dedc08366ba2ff965f97be93bc8c55
rikkeuldbaek <rikke_uldbaek@hotmail.com>
2023-05-04 18:17:37 +0200

    data works

--- src_2/data.py
+++ src_2/data.py
@@ -1,17 +1,53 @@
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Dense, Activation,Dropout,Conv2D, MaxPooling2D,BatchNormalization, Flatten
+from tensorflow.keras.optimizers import Adam, Adamax
+from tensorflow.keras.metrics import categorical_crossentropy
+from tensorflow.keras import regularizers
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Model, load_model, Sequential
+import numpy as np
+import pandas as pd
+import shutil
+import time
+import cv2 as cv2
+from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
 import os
-
-#count length of images
-
-path = os.path.join(os.getcwd(),"images")
-
-count = 0
-# Iterate directory
-for i in os.listdir(path):
-    # check if current path is a file
-    if os.path.isfile(os.path.join(path, i)):
-        count += 1
-print('File count:', count)
+import seaborn as sns
+sns.set_style('darkgrid')
+from PIL import Image
+from sklearn.metrics import confusion_matrix, classification_report
 
 
-#for i in os.listdir(path):
-    
+
+################# FIXING THE GROUNDTRUTH.CSV #################
+
+
+df=pd.read_csv(os.path.join(os.getcwd(),"data","archive","GroundTruth.csv") )
+print (df.head())
+print (len(df))
+print (df.columns)
+labels=['MEL', 'NV', 'BCC', 'AKIEC', 'BKL', 'DF', 'VASC'] # Labels of diagnostic skin cancer types
+
+# Insert missing .jpg after image filename
+df['image']=df['image'].apply(lambda x: x+ '.jpg')
+print (df.head())
+
+# Rearrange dataframe and make a label column with type of skin cancer
+label_list=[]
+for i in range (len(df)):
+    row= list(df.iloc[i])
+    del row[0]
+    index=np.argmax(row)
+    label=labels[index]
+    label_list.append(label)
+df['label']= label_list
+df=df.drop(labels, axis=1)
+print (df.head())
+
+
--- src_2/unzip_data.py
+++ src_2/unzip_data.py
@@ -12,96 +12,33 @@ from zipfile import ZipFile
 import os
 #import pandas as pd
 
+##################### UNZIPPING #######################
+
 #define path for zipfile of images
 skin_cancer_path="/work/VA_final_project/skin_cancer/archive.zip"
-skin_cancer_path2="/work/VA_final_project/skin_cancer/archive_new.zip"
-
-#unzip  and extract in current directory
-#with ZipFile(skin_cancer_path, 'r') as f:
-#    f.extractall()
-
-
-
-filename = skin_cancer_path
-
-import zipfile
-
-#try:
-#    z = zipfile.ZipFile(filename)
-#except zipfile.BadZipfile:
-#    import commands
-#    commands.getoutput('zip -FF '+filename)
-#    z = zipfile.ZipFile(filename)
-
-#for i in z.infolist():
-#    print(i.filename, i.file_size)
-
-
-#try: 
-#    z.read('images/ISIC_0026299.jpg')
-#except zipfile.BadZipfile:
-#    print('Bad CRC-32')
-
-
-#with zipfile.ZipFile(skin_cancer_path, mode="r") as archive:
-#    archive.printdir()
-
-
-#see if there are any corrupted files
-try:
-    with zipfile.ZipFile(skin_cancer_path2) as archive:
-        archive.printdir()
-except zipfile.BadZipFile as error:
-    print(error) #seems like there are not 
-
-# print info of a file
-#with zipfile.ZipFile(skin_cancer_path, mode="r") as archive:
-#     info = archive.getinfo("images/ISIC_0026299.jpg")
-#     print(info)
      
 
 ####### reading in files
 with zipfile.ZipFile(skin_cancer_path, mode="r") as archive:
     badfiles=archive.testzip()
-    print(badfiles)
-    #archive.extractall("data/")
-
-
-import subprocess
-import zipfile
-
-zin = zipfile.ZipFile (skin_cancer_path, 'r')
-zout = zipfile.ZipFile (skin_cancer_path2, 'w')
-for item in zin.infolist():
-    buffer = zin.read(item.filename)
-    print(buffer)
-    if (item.filename[-4:] != '0026299.jpg'):
-        zout.writestr(item, buffer)
-zout.close()
-zin.close()
-
-#trying to extract new files minus deleted one
-with zipfile.ZipFile(skin_cancer_path, mode="r") as archive:
-    archive.extractall("data/") #save in data folder
-
-
-
-#filename = skin_cancer_path
-
-#import zipfile
-
-#z = zipfile.ZipFile(filename)
-
-#for i in z.infolist():
-#    print i.filename, i.file_size
-
-#z.read('somefile')
-
-
-
-
-
+    print(badfiles) #print if any files are corrupted
+    archive.extractall("data/")
 
 
 #images/ISIC_0026299.jpg' 
 
+##################### COUNTING DATA #######################
+# check all data is loaded in
+# define path of images
+path = os.path.join(os.getcwd(),"data","archive","images") 
+
+count = 0
+
+# Iterate directory
+for i in os.listdir(path):
+    # check if current path is a file
+    if os.path.isfile(os.path.join(path, i)):
+        count += 1
+print('File count:', count) # 10017 images
+
+    
